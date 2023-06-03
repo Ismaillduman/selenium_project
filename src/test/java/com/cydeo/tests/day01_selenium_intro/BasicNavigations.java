bdb79ad408ac5711e2e3d9540c814067eb28f175
Ismailduman <issmailduman@gmail.com>
2023-06-03 23:51:12 +0200

    last fix

---
+++ src/test/java/com/cydeo/tests/day01_selenium_intro/BasicNavigations.java
@@ -0,0 +1,39 @@
+package com.cydeo.tests.day01_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://www.tesla.com");
+        driver.manage().window().maximize();
+        driver.navigate().back();
+
+            Thread.sleep(2000);
+
+
+        driver.navigate().forward();
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+
+        }
+        driver.navigate().refresh();
+        String current_title=driver.getTitle();
+        System.out.println("current Title = " + current_title);
+        String current_url=driver.getCurrentUrl();
+        System.out.println("current_url = " + current_url);
+
+        Thread.sleep(1000);
+        driver.navigate().to("https://www.google.com");
+        Thread.sleep(2000);
+
+        driver.close();
+
+    }
+}
