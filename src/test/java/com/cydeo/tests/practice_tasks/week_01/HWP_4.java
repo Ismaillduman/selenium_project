7432324e4d8ea6e7b243b26fdf92352b0d4baacd
Ismailduman <issmailduman@gmail.com>
2023-06-05 15:04:26 +0200

    new tasks HWP 2,3,4

---
+++ src/test/java/com/cydeo/tests/practice_tasks/week_01/HWP_4.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.practice_tasks.week_01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class HWP_4 {
+    public static void main(String[] args) {
+        /*TC #4: Practice Cydeo – Class locator practice
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/inputs
+3- Click to “Home” link
+4- Verify title is as expected: Expected: Practice
+PS: Locate “Home” link using “className” locator*/
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://practice.cydeo.com/inputs");
+        WebElement home= driver.findElement(By.className("nav-link"));
+        home.click();
+        String currentTitle=driver.getTitle();
+        String expectedTitle= "Practice";
+        Assert.assertEquals(currentTitle,expectedTitle);
+        driver.quit();
+
+    }
+}
