fdbf0f369422d4a411b00db4f822e656d96c1e97
Ismailduman <issmailduman@gmail.com>
2023-06-16 00:45:37 +0200

    configuration properties test

---
+++ src/test/java/com/cydeo/tests/day_08webTablesCont_properties_configReader/WebElements.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.day_08webTablesCont_properties_configReader;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebElements {
+     WebDriver driver;
+
+
+}
