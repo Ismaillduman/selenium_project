5127c5673473ac8b7aec613536cf329ba4d0ff3b
Ismailduman <issmailduman@gmail.com>
2023-07-05 21:27:41 +0200

    webtable

--- src/test/java/com/cydeo/tests/office_hours/week_3/WebTable.java
+++ src/test/java/com/cydeo/tests/office_hours/week_3/WebTable.java
@@ -23,17 +23,29 @@ public class WebTable {
     public void webTable_test() {
 
         Driver.getDriver().get(" https://seleniumpractise.blogspot.com/2016/08/how-to-handle-calendar-in-selenium.html");
-        Driver.getDriver().findElement(By.id("[id='datepicker']")).click();
+        Driver.getDriver().findElement(By.id("datepicker")).click();
 
-        List<WebElement> allDays= Driver.getDriver().findElements(By.xpath("//table[@class='uui-datepicker-calendar']//td"));
+        List<WebElement> allDays = Driver.getDriver().findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+
+//        for (WebElement eachDay : allDays) {
+//            String day=eachDay.getText();
+//            System.out.println(eachDay);
+//            if(day.equalsIgnoreCase("5")){
+//                eachDay.click();
+//                break;
+//            }
+//        }
 
         for (WebElement eachDay : allDays) {
-            String day=eachDay.getText();
-            System.out.println(eachDay);
-            if(day.equalsIgnoreCase("5")){
-                eachDay.click();
-                break;
+            String day = eachDay.getText();
+
+            if (day.isBlank()||day.equalsIgnoreCase("5")) {
+                continue;
             }
+            System.out.println(day);
+             eachDay.click();
+
+
         }
 
 
